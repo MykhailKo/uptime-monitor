c0dc837db112836dfee037acd2ae68faf6e0aa81
MykhailKo <tuareg1812@gmail.com>
2021-07-18 18:18:00 +0300

    initialized routing

--- index.js
+++ index.js
@@ -1,16 +1,55 @@
 const http = require('http');
 const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 var server = http.createServer((req, res) => {
-  var parsedUrl = url.parse(req.url, true);
+  const parsedUrl = url.parse(req.url, true);
+  const decoder = new StringDecoder('utf-8');
 
-  var path = parsedUrl.pathname;
+  var path = parsedUrl.pathname;  
   var trimmedPath = path.replace(/^\/+|\/+$/g, '');
-  res.end('Hello world!\n');
-
-  console.log(`Request id received on path: ${trimmedPath}`);
+  var queryStringObject = parsedUrl.query;
+  var method = req.method.toLocaleLowerCase();
+  var headers = req.headers;
+  var buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+    var data = {
+      'trimmedPath': trimmedPath,
+      'queryStringObject': queryStringObject,
+      'method': method,
+      'headers': headers,
+      'payload': buffer
+    };
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+      payload = typeof(payload) === 'object' ? payload : {};
+      var payloadString = JSON.stringify(payload);
+      res.writeHead(statusCode);
+      res.end(payloadString);
+      console.log(`Returning response: ${statusCode} ${payloadString}`); 
+    });    
+  }); 
 });
 
 server.listen(3000, () => {
   console.log('The server is listening on port 3000');
 });
+
+var handlers = {};
+
+handlers.sample = function(data, callback) {
+  callback(406, {'name': 'sample handler'});
+};
+
+handlers.notFound = function(data, callback) {
+  callback(404);
+};
+
+const router = {
+  'sample': handlers.sample
+};
