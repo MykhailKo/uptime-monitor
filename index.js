2f4a4ed99a6784fbb441bb8d750abf8b17f3aee1
MykhailKo <tuareg1812@gmail.com>
2021-07-18 18:52:59 +0300

    added configuration

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const http = require('http');
 const url = require('url');
 const { StringDecoder } = require('string_decoder');
 
+const config = require('./config');
+
 var server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   const decoder = new StringDecoder('utf-8');
@@ -30,14 +32,15 @@ var server = http.createServer((req, res) => {
       payload = typeof(payload) === 'object' ? payload : {};
       var payloadString = JSON.stringify(payload);
       res.writeHead(statusCode);
+      res.setHeader('Content-Type', 'application/json');
       res.end(payloadString);
       console.log(`Returning response: ${statusCode} ${payloadString}`); 
     });    
   }); 
 });
 
-server.listen(3000, () => {
-  console.log('The server is listening on port 3000');
+server.listen(config.port, () => {
+  console.log(`The server is listening on port ${config.port} in ${config.envName} mode.`);
 });
 
 var handlers = {};
